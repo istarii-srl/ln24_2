84182cd1c0fa43ac29d99a607c325566a8434342
florvhee <florvhee@gmail.com>
2022-01-18 11:47:35 +0100

    debug

--- ln/__manifest__.py
+++ ln/__manifest__.py
@@ -17,7 +17,7 @@
     # for the full list
     'category': 'ERP',
     'license': 'OPL-1',
-    'version': '15.0.0.129',
+    'version': '15.0.0.130',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'planning', 'contacts', 'hr_attendance', 'hr', 'hr_holidays'],
