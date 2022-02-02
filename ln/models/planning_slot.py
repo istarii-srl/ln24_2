48af27fa5df81f1d427f0bc036e373c1cd032f0c
florvhee <florvhee@gmail.com>
2022-02-02 19:41:15 +0100

    debug

--- ln/models/planning_slot.py
+++ ln/models/planning_slot.py
@@ -4,34 +4,12 @@ class PlanningSlot(models.Model):
     _name = "planning.slot"
     _inherit = "planning.slot"
 
-    #role_id = fields.Many2one(domain=lambda self: [('id', 'in', self._get_role_ids())])
-    #resource_id = fields.Many2one(domain= lambda self: [('employee_single_id', 'in', self._get_resource_ids())])
     slot_to_confirm = fields.Boolean(string="Slot à confirmer", default=True)
     confirm_status = fields.Selection(string="Statut de confirmation", selection=[("to_confirm", "À confirmer"), ('refused', 'Refusé'), ("confirmed", 'Confirmé')], default="to_confirm", readonly=False)
     has_synced = fields.Boolean(string="Est sync avec présence", default=False)
     has_rest = fields.Boolean(string="Pause dans le shift", compute="_compute_rest")
     rest_time = fields.Float(string="Temps de pause", compute="_compute_rest")
     synchro_attendance = fields.Boolean(string="Synchronisation avec Attendance", default=True)
-    fake_compute = fields.Boolean(compute="_compute_domain")
-
-    @api.depends('synchro_attendance')
-    def _compute_domain(self):
-        for slot in self:
-            slot.fake_compute = True
-            slot.write({'role_id': slot.role_id.id, 'resource_id':slot.resource_id.id})
-
-
-    def _get_role_ids(self):
-        if self.resource_id:
-            return self.resource_id.employee_single_id.planning_role_ids.ids
-        else:
-            return self.env["planning.role"].search([]).ids
-    
-    def _get_resource_ids(self):
-        if self.role_id:
-            return self.role_id.employee_ids.ids
-        else:
-            return self.env["hr.employee"].search([]).ids
 
                  
 
